bff0147fccdd5efd5902d6600e5b5abcae215096
jbaillai42 <jobailla@student.42.fr>
2018-03-22 22:16:38 +0100

    add

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 1337;
+
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// test route
+app.get('/', function (req, res) { res.status(200).send('Bonjour !'); });
+
+app.listen(port, function () {
+	console.log('Listening on port ' + port);
+});
+
+function createPayload (text) {
+	return ({text});
+}
+
+app.post('/hello', function (req, res, next) {
+	var userName = req.body.user_name;
+
+	// Loop otherwise..
+	if (userName !== 'slackbot') {
+		return res.status(200).json(createPayload('Bonjour ' + userName + ' :smile:'));
+	} else {
+		return res.status(200).end();
+	}
+});
+
+app.post('/bye', function (req, res, next) {
+	var userName = req.body.user_name;
+
+	if (userName !== 'slackbot') {
+		return res.status(200).json(createPayload('Aurevoir ' + userName + ' :upside_down_face:'));
+	} else {
+		return res.status(200).end();
+	}
+});
